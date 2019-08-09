d902461ff1c302d274a1a157d8b136ba7f598f92
Devesh <dvdramp@gmail.com>
2019-08-09 13:13:30 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import {
+  MatCardModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatIconModule
+} from '@angular/material';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { ImageGalleryComponent } from './admin/image-gallery/image-gallery.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    ImageGalleryComponent
   ],
   imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+  ], 
   providers: [],
   bootstrap: [AppComponent]
 })
